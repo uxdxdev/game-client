ac437e4ecc390da1b316a8fe865ba9ab44765477
david.morton <david.morton@servicenow.com>
2021-12-30 23:36:33 +0000

    added a drawing canvas for all authed clients

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import io from 'socket.io-client';
 import { initializeApp } from 'firebase/app';
@@ -8,6 +8,7 @@ import {
   getRedirectResult,
   GoogleAuthProvider,
 } from 'firebase/auth';
+import './styles.css';
 
 // FIREBASE
 const firebaseConfig = {
@@ -28,6 +29,7 @@ const events = {
   NUMBER_OF_CONNECTED_CLIENTS: 'number_of_connected_clients',
   CONNECTED: 'connected',
   CONNECTION_ERROR: 'connect_error',
+  DRAW: 'draw',
 };
 
 const App = () => {
@@ -35,8 +37,85 @@ const App = () => {
   const [numberOfClients, setNumberOfClients] = useState(0);
   const [authToken, setAuthToken] = useState(null);
   const [userId, setUserId] = useState(null);
-  const [isAuthenticated, setIsAuthenticated] = useState(null);
   const [isServerAuthed, setIsServerAuthed] = useState(null);
+  const [color, setColor] = useState('#000000');
+
+  const x = useRef();
+  const y = useRef();
+  const isDrawing = useRef();
+  const drawingCanvas = useRef();
+  const canvasContext = useRef();
+  const canvasContainer = useRef();
+
+  useEffect(() => {
+    setColor('#' + (Math.random().toString(16) + '00000').slice(2, 8));
+  }, []);
+
+  const drawLine = ({ x1, y1, x2, y2, strokeColor }) => {
+    canvasContext.current.beginPath();
+    canvasContext.current.strokeStyle = strokeColor;
+    canvasContext.current.lineWidth = 5;
+    canvasContext.current.moveTo(x1, y1);
+    canvasContext.current.lineTo(x2, y2);
+    canvasContext.current.stroke();
+    canvasContext.current.closePath();
+  };
+
+  useEffect(() => {
+    const canvas = drawingCanvas.current;
+    const context = canvas.getContext('2d');
+    canvasContext.current = context;
+
+    const handleMouseDown = (e) => {
+      x.current = e.offsetX;
+      y.current = e.offsetY;
+      isDrawing.current = true;
+    };
+    canvas.addEventListener('pointerdown', handleMouseDown);
+
+    const handleMouseMove = (e) => {
+      if (isDrawing.current === true) {
+        const data = {
+          x1: x.current,
+          y1: y.current,
+          x2: e.offsetX,
+          y2: e.offsetY,
+          strokeColor: color,
+        };
+        drawLine(data);
+        x.current = e.offsetX;
+        y.current = e.offsetY;
+
+        if (socketClient) {
+          socketClient.emit(events.DRAW, data);
+        }
+      }
+    };
+    canvas.addEventListener('pointermove', handleMouseMove);
+
+    const handleMouseUp = (e) => {
+      if (isDrawing.current === true) {
+        x.current = 0;
+        y.current = 0;
+        isDrawing.current = false;
+      }
+    };
+    window.addEventListener('pointerup', handleMouseUp);
+
+    const handleWindowResize = () => {
+      canvas.width = canvasContainer.current?.clientWidth;
+      canvas.height = canvasContainer.current?.clientHeight;
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      canvas.removeEventListener('pointerdown', handleMouseDown);
+      canvas.removeEventListener('pointermove', handleMouseMove);
+      window.removeEventListener('pointerup', handleMouseUp);
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, [color, socketClient]);
 
   useEffect(() => {
     auth.onAuthStateChanged(async (user) => {
@@ -44,11 +123,9 @@ const App = () => {
         setUserId(user.uid);
         const token = await user.getIdToken();
         setAuthToken(token);
-        setIsAuthenticated(true);
       } else {
         setUserId(null);
         setAuthToken(null);
-        setIsAuthenticated(false);
       }
     });
   }, []);
@@ -60,14 +137,14 @@ const App = () => {
     });
   }, []);
 
-  // ping the server to check if it's up
+  // once authenticated, ping the server to check if it's up
   useEffect(() => {
-    if (isAuthenticated) {
+    if (authToken) {
       fetch(`${process.env.REACT_APP_SERVER_URL}/ping`).catch((err) =>
         console.log(err)
       );
     }
-  }, [isAuthenticated]);
+  }, [authToken]);
 
   // connect to the websocket server and store socket connection
   useEffect(() => {
@@ -90,6 +167,14 @@ const App = () => {
     if (socketClient) {
       socketClient.on(events.CONNECTED, (clientId) => {
         setIsServerAuthed(!!clientId);
+      });
+    }
+  }, [socketClient]);
+
+  useEffect(() => {
+    if (socketClient) {
+      socketClient.on(events.DRAW, (data) => {
+        drawLine(data);
       });
     }
   }, [socketClient]);
@@ -141,37 +226,41 @@ const App = () => {
   };
 
   return (
-    <>
-      <div>firebase authenticated? {isAuthenticated ? 'true' : 'false'}</div>
-      <button
-        onClick={handleAuth}
-        disabled={isAuthenticated === null || isAuthenticated}
-      >
-        Login to Firebase
-      </button>
-      <button
-        onClick={handleUnAuth}
-        disabled={isAuthenticated === null || !isAuthenticated}
-      >
-        Logout of Firebase
-      </button>
-      <div>server authenticated? {isServerAuthed ? 'true' : 'false'}</div>
-      <button
-        onClick={handleSocketConnect}
-        disabled={!isAuthenticated || isServerAuthed === null || isServerAuthed}
-      >
-        Connect to Socket.io server
-      </button>
-      <button
-        onClick={handleSocketDisconnect}
-        disabled={
-          !isAuthenticated || isServerAuthed === null || !isServerAuthed
-        }
-      >
-        Disconnect from Socket.io server
-      </button>
-      <div>{numberOfClients} currently connected</div>
-    </>
+    <div className="h-full">
+      <header>
+        <div>firebase authenticated? {authToken ? 'true' : 'false'}</div>
+        <button onClick={handleAuth} disabled={authToken}>
+          Login to Firebase
+        </button>
+        <button
+          onClick={handleUnAuth}
+          disabled={authToken === null || !authToken}
+        >
+          Logout of Firebase
+        </button>
+        <div>server authenticated? {isServerAuthed ? 'true' : 'false'}</div>
+        <button
+          onClick={handleSocketConnect}
+          disabled={!authToken || isServerAuthed === null || isServerAuthed}
+        >
+          Connect to Socket.io server
+        </button>
+        <button
+          onClick={handleSocketDisconnect}
+          disabled={!authToken || isServerAuthed === null || !isServerAuthed}
+        >
+          Disconnect from Socket.io server
+        </button>
+        <div>{numberOfClients} currently connected</div>
+      </header>
+      <div id="canvas-container" ref={canvasContainer}>
+        <canvas
+          ref={drawingCanvas}
+          width={canvasContainer.current?.clientWidth}
+          height={canvasContainer.current?.clientHeight}
+        ></canvas>
+      </div>
+    </div>
   );
 };
 
